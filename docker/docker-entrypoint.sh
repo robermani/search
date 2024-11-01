5840b38644a089080403d375a3659024c0e50e2d
alpa-ilan-r <ilan.r@alpa-cosmetics.co.il>
2024-11-01 18:06:36 +0200

    Fix Docker configurations

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 set -e
 
-# Run Composer install
-composer install
+echo "Entrypoint script is running"
 
-# Run npm install
-npm install
-
-# Build assets
-npm run dev
-
+# Execute the container's main process (what's set as CMD in the Dockerfile)
 exec "$@"
